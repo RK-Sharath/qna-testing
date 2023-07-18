47be2dbbb4c186cd2994f1cda5977c9f39f1b5b0
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-18 12:14:43 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-from src.schemas.config import Configuration
-from src.schemas.params import Parameters
-from src.schemas.engine import Engine
-from src.schemas.chroma import MiniLML6V2EmbeddingFunctionLangchain, ChromaWithUpsert
+import Configuration
+import Parameters
+import Engine
+import MiniLML6V2EmbeddingFunctionLangchain, ChromaWithUpsert
 
 __all__ = [
     "Configuration",
